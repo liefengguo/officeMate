dd9641c18b511f55ee151c5ff4964c69d29f2ca3
liefengguo <1321755404@qq.com>
2025-04-28 15:13:36 +0800

    修复bug

--- app/project_page.py
+++ app/project_page.py
@@ -69,7 +69,7 @@ class ProjectPage(QWidget):
     def handle_snapshot_created(self, file_path):
         """当快照创建完成后，刷新所有需要的页面"""
         if hasattr(self, 'page_history'):
-            self.page_history.list_widget.load_snapshots()
+            self.page_history.load_snapshots()
         if hasattr(self, 'page_compare'):
             self.page_compare.list_widget.load_snapshots()
 
